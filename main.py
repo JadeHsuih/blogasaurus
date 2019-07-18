e0540fffca70db4932e90702be1bdd93fd590400
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-18 10:35:23 -0400

    Added new and view post pages

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ class MainPage(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render())
 
+class BlogHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/new_post.html')
+        self.response.write(template.render())
+
+    def post(self):
+        template = jinja_env.get_template('templates/view_post.html')
+        title_var = self.request.get('title')
+        content_var = self.request.get('content')
+        author_var = self.request.get('aname')
+        dict = {
+            'titlev': title_var,
+            'contentv': content_var,
+            'anamev': author_var
+        }
+        self.response.write(template.render(dict))
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/newp', BlogHandler),
 ], debug=True)
